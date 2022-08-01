4b88bcf670bcf383644c63e52925dc6e389f701e
Lenka Wolfova <lenka.wolfova@grhapp.com>
2022-08-01 17:02:06 +0100

    Changed input questions to wording questions

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from calc import monthly_compounding
 
 
 def app():
-    initial = float(input("Please input initial value: "))
+    initial = float(input("How much money are you starting with: "))
     monthly = float(input("Please input the value of consistent monthly contributions: "))
     years = int(input("Please input how many years you will be invested for: "))
-    annual_rate = float(input("Please input the annual percent floaterest rate, as a decimal between 0 and 1: "))
+    annual_rate = float(input("How many years will you be investing for: "))
     final_sum = monthly_compounding(initial, monthly, years, annual_rate)
     print(f"Your final value is {final_sum}")
 
