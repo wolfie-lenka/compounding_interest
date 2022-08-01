4646161ebde6e2cb61c79e3fda958c95e75bfab0
Lenka Wolfova <lenka.wolfova@grhapp.com>
2022-08-01 17:25:14 +0100

    adding the correct file

--- test_calc.py
+++ test_calc.py
@@ -5,8 +5,6 @@ Created on Fri Jul 29 18:57 2022
 
 @author: Lenka Wolfova
 """
-
-
 def test_zeros():
     # Arrange
     initial_investment = 0
@@ -16,10 +14,21 @@ def test_zeros():
     expected_output = 0
 
     # Act
-    actual_output = monthly_compounding()
+    actual_output = monthly_compounding(initial_investment, interest_rate, monthly_deposit, years)
 
     #Assert
     assert actual_output == expected_output
 
 def test_one_year_zero_interest ():
-    pass
+     # Arrange
+    initial_investment = 100
+    interest_rate = 0
+    monthly_deposit = 0
+    years = 1
+    expected_output = 0
+
+    # Act
+    actual_output = monthly_compounding(initial_investment, interest_rate, monthly_deposit, years)
+
+    #Assert
+    assert actual_output == expected_output
