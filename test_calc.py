c5875313e02010640af42e97d7d9248915b3dcd9
Lenka Wolfova <lenka.wolfova@grhapp.com>
2022-08-01 17:29:25 +0100

    this is the correct version

--- test_calc.py
+++ test_calc.py
@@ -1,34 +1,140 @@
-# -*- coding: utf-8 -*-
-""" 
+from calc import monthly_compounding
 
-Created on Fri Jul 29 18:57 2022
 
-@author: Lenka Wolfova
-"""
 def test_zeros():
     # Arrange
-    initial_investment = 0
-    interest_rate = 0
-    monthly_deposit = 0
+    initial = 0
+    monthly = 0
     years = 0
-    expected_output = 0
+    annual_rate = 0
+    expected_sum = 0
 
     # Act
-    actual_output = monthly_compounding(initial_investment, interest_rate, monthly_deposit, years)
+    final_sum = monthly_compounding(initial, monthly, years, annual_rate)
 
-    #Assert
-    assert actual_output == expected_output
+    # Assert
+    assert final_sum == expected_sum
 
-def test_one_year_zero_interest ():
-     # Arrange
-    initial_investment = 100
-    interest_rate = 0
-    monthly_deposit = 0
+
+def test_initial_only_one_year_zero_apr():
+    # Arrange
+    initial = 5000
+    monthly = 0
     years = 1
-    expected_output = 0
+    annual_rate = 0
+    expected_sum = 5000
 
     # Act
-    actual_output = monthly_compounding(initial_investment, interest_rate, monthly_deposit, years)
+    final_sum = monthly_compounding(initial, monthly, years, annual_rate)
 
-    #Assert
-    assert actual_output == expected_output
+    # Assert
+    assert final_sum == expected_sum
+
+
+def test_initial_monthly_one_year_zero_apr():
+    # Arrange
+    initial = 5000
+    monthly = 500
+    years = 1
+    annual_rate = 0
+    expected_sum = 11000
+
+    # Act
+    final_sum = monthly_compounding(initial, monthly, years, annual_rate)
+
+    # Assert
+    assert final_sum == expected_sum
+
+
+def test_initial_monthly_multiple_years_zero_apr():
+    # Arrange
+    initial = 5000
+    monthly = 500
+    years = 5
+    annual_rate = 0
+    expected_sum = 35000
+
+    # Act
+    final_sum = monthly_compounding(initial, monthly, years, annual_rate)
+
+    # Assert
+    assert final_sum == expected_sum
+
+
+def test_initial_monthly_multiple_years():
+    # Arrange
+    initial = 5000
+    monthly = 500
+    years = 5
+    annual_rate = 0.05
+    expected_sum = 40288.28
+
+    # Act
+    final_sum = monthly_compounding(initial, monthly, years, annual_rate)
+
+    # Assert
+    assert final_sum == expected_sum
+
+
+def test_initial_monthly_one_year():
+    # Arrange
+    initial = 5000
+    monthly = 500
+    years = 1
+    annual_rate = 0.05
+    expected_sum = 11386.29
+
+    # Act
+    final_sum = monthly_compounding(initial, monthly, years, annual_rate)
+
+    # Assert
+    assert final_sum == expected_sum
+
+
+def test_initial_only_one_year():
+    # Arrange
+    initial = 5000
+    monthly = 0
+    years = 1
+    annual_rate = 0.05
+    expected_sum = 5250
+
+    # Act
+    final_sum = monthly_compounding(initial, monthly, years, annual_rate)
+
+    # Assert
+    assert final_sum == expected_sum
+
+
+def test_initial_monthly():
+    # Arrange
+    initial = 5000
+    monthly = 500
+    years = 0
+    annual_rate = 0.05
+    expected_sum = 5000
+
+    # Act
+    final_sum = monthly_compounding(initial, monthly, years, annual_rate)
+
+    # Assert
+    assert final_sum == expected_sum
+
+
+def test_bad_apr():
+    # Arrange
+    initial = 0
+    monthly = 0
+    years = 0
+    annual_rate = 3
+    expected_sum = "Error: annual_rate is a percentage and so must be between 0 and 1"
+
+    # Act
+    final_sum = monthly_compounding(initial, monthly, years, annual_rate)
+
+    # Assert
+    assert final_sum == expected_sum
+
+
+def test_bad_years():
+    pass
